16fdfb021c3cf24082070542d269e6aa6926fea0
Amin <amin030296@gmail.com>
2018-06-13 12:22:11 +0200

    Auth

--- unityProject/Assets/Scripts/Authenication.cs
+++ unityProject/Assets/Scripts/Authenication.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Firebase.Auth;
+
 
 public class Authenication : MonoBehaviour 
 {
-
+	FirebaseAuth auth;
 
 	#region  UI Elements
 	[Header("UI Elements")]
@@ -13,22 +15,51 @@ public class Authenication : MonoBehaviour
 	[Header("Sign In")]
 	[SerializeField] private InputField EmailSignIn;
 	[SerializeField] private InputField PasswordSignIn;
+	[SerializeField] private InputField SignInButton;
 
 	[Header("Sign Up")]
 	[Space(5)]
 	[SerializeField] private InputField EmailSignUp;
 	[SerializeField] private InputField PasswordSignUp;
 	[SerializeField] private InputField ConfirmPassword;
+	[SerializeField] private Button SignUpButton;
 	#endregion
 
+	[SerializeField] private Text DebugText;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		auth = FirebaseAuth.DefaultInstance;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+
+	public void SignUp ()
+	{
+		auth.CreateUserWithEmailAndPasswordAsync(EmailSignUp.text,ConfirmPassword.text).ContinueWith(task => 
+		{
+			 if (task.IsCanceled)
+            {
+                DebugText.text = ("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                DebugText.text =("CreateUserWithEmailAndPasswordAsync error: " + task.Exception);
+                if (task.Exception.InnerExceptions.Count > 0)
+                DebugText.text = (task.Exception.InnerExceptions[0].Message);
+                return;
+            }
+			
+			FirebaseUser newUser = task.Result;
+
+			DebugText.text = "Welcome : " + newUser.Email;
+
+
+		});
+
+
+		//after sign in Button Interaction is Disable
 	}
 }
