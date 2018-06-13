cae68961dc14b0921a7a8086e8badf69b6001489
Amin <amin@lemonorange.pl>
2018-06-13 11:36:17 +0200

    Auth Element Added

---
+++ unityProject/Assets/Scripts/Authenication.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Authenication : MonoBehaviour 
+{
+
+
+	#region  UI Elements
+	[Header("UI Elements")]
+	[Space(5)]
+	[Header("Sign In")]
+	[SerializeField] private InputField EmailSignIn;
+	[SerializeField] private InputField PasswordSignIn;
+
+	[Header("Sign Up")]
+	[Space(5)]
+	[SerializeField] private InputField EmailSignUp;
+	[SerializeField] private InputField PasswordSignUp;
+	[SerializeField] private InputField ConfirmPassword;
+	#endregion
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
