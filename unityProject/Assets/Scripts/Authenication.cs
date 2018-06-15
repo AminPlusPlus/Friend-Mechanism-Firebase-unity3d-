45cdb7d0f2057208504de41323e1f7107918326b
Amin <amin030296@gmail.com>
2018-06-15 13:14:35 +0200

    save database user

--- unityProject/Assets/Scripts/Authenication.cs
+++ unityProject/Assets/Scripts/Authenication.cs
@@ -3,53 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Firebase.Auth;
+using Firebase.Database;
 
 
-public class Authenication : MonoBehaviour 
+public class Authenication : MonoBehaviour
 {
-	FirebaseAuth auth;
+    FirebaseAuth auth;
+    DatabaseReference dbRef;
 
-	#region  UI Elements
-	[Header("Registration UI Elements")]
-	[Space(5)]
-	[SerializeField] private InputField EmailSignUp;
-	[SerializeField] private InputField PasswordSignUp;
-	[SerializeField] private Button SignUpButton;
-	#endregion
+    #region  UI Elements
+    [Header("Registration UI Elements")]
+    [Space(5)]
+    [SerializeField] private InputField EmailSignUp;
+    [SerializeField] private InputField PasswordSignUp;
+    [SerializeField] private Button SignUpButton;
+    #endregion
 
-	[SerializeField] private Text DebugText;
+    [SerializeField] private Text DebugText;
 
-	// Use this for initialization
-	void Start () 
-	{
-		auth = FirebaseAuth.DefaultInstance;
-	}
-	
-	
-
-	public void SignUp ()
-	{
-		auth.CreateUserWithEmailAndPasswordAsync(EmailSignUp.text,PasswordSignUp.text).ContinueWith(task => 
-		{
-			 if (task.IsCanceled)
-            {
-                DebugText.text = ("CreateUserWithEmailAndPasswordAsync was canceled.");
-                return;
-            }
-            if (task.IsFaulted)
-            {
-                DebugText.text =("CreateUserWithEmailAndPasswordAsync error: " + task.Exception);
-                if (task.Exception.InnerExceptions.Count > 0)
-                DebugText.text = (task.Exception.InnerExceptions[0].Message);
-                return;
-            }
-			
-			FirebaseUser newUser = task.Result;
-
-			DebugText.text = "Welcome : " + newUser.Email;
-		});
+    // Use this for initialization
+    void Start()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+        dbRef = FirebaseDatabase.DefaultInstance.RootReference;
 
 
-		//after sign in Button Interaction is Disable
-	}
+    }
+
+
+
+    public void SignUp()
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(EmailSignUp.text, PasswordSignUp.text).ContinueWith(task =>
+         {
+             if (task.IsCanceled)
+             {
+                 DebugText.text = ("CreateUserWithEmailAndPasswordAsync was canceled.");
+                 return;
+             }
+             if (task.IsFaulted)
+             {
+                 DebugText.text = ("CreateUserWithEmailAndPasswordAsync error: " + task.Exception);
+                 if (task.Exception.InnerExceptions.Count > 0)
+                     DebugText.text = (task.Exception.InnerExceptions[0].Message);
+                 return;
+             }
+
+             FirebaseUser newUser = task.Result;
+
+             Dictionary<string, object> userData = new Dictionary<string, object>()
+             {
+                {"email" , newUser.Email}
+             };
+
+			 //Save to DB
+			dbRef.Child(DataService.Instance.RefUser).Child(newUser.UserId).SetValueAsync(userData);
+
+             DebugText.text = "Welcome : " + newUser.Email;
+         });
+
+    }
+
+    public void SignIn()
+    {
+        auth.SignInWithEmailAndPasswordAsync(EmailSignUp.text, PasswordSignUp.text).ContinueWith(task =>
+         {
+             if (task.IsCanceled)
+             {
+                 DebugText.text = ("CreateUserWithEmailAndPasswordAsync was canceled.");
+                 return;
+             }
+             if (task.IsFaulted)
+             {
+                 DebugText.text = ("CreateUserWithEmailAndPasswordAsync error: " + task.Exception);
+                 if (task.Exception.InnerExceptions.Count > 0)
+                     DebugText.text = (task.Exception.InnerExceptions[0].Message);
+                 return;
+             }
+
+             FirebaseUser newUser = task.Result;
+
+             DebugText.text = "Welcome : " + newUser.Email;
+         });
+
+    }
 }
---
+++ unityProject/Assets/Scripts/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public sealed class DataService 
+{	
+
+	//SINGELTON
+	static readonly DataService _instance = new DataService();
+	public static DataService Instance {get {return _instance;}}
+
+	//REFERENCES
+	private string refUser = "Users";
+	public string RefUser { get {return refUser;}  }
+	
+}
