d18a231f4d55098e1e920aba7d08305fb0639983
Amin <amin030296@gmail.com>
2018-06-18 13:09:41 +0200

    profile User

---
+++ unityProject/Assets/Scripts/View/ProfileUser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileUser : MonoBehaviour 
+{
+	[Header("UI Elements")]
+	[SerializeField] private Image profileIcon;
+	[SerializeField] private Text fullName;
+
+
+	/// <summary>
+	/// Initilization UI elements from DB
+	/// </summary>
+	void initView ()
+	{
+
+	}
+	
+}
